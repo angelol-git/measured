02b01c20d7f407c29c57dc50f1d405a9ed2a2928
aladores <angelo.ladores@outlook.com>
2023-08-01 14:13:12 -0400

    add import error message

--- src/components/header/settings/Data.jsx
+++ src/components/header/settings/Data.jsx
@@ -3,6 +3,7 @@ import "./Data.css";
 
 function Data(props) {
   const [importMessage, setImportMessage] = useState("");
+  const [importError, setImportError] = useState("");
   const exportLength = Object.keys(props.itemData).length;
 
   function handleExportClick() {
@@ -23,15 +24,27 @@ function Data(props) {
     const reader = new FileReader();
 
     reader.onload = () => {
+      console.log(event.target.files[0].type);
+
       try {
         const jsonObject = JSON.parse(reader.result);
         const importLength = Object.keys(jsonObject).length;
-
+        setImportError(``);
         setImportMessage(`Imported ${importLength} Items`);
         console.log(importMessage);
         props.handleImport(jsonObject);
       } catch (error) {
-        console.error("Error parsing JSON:", error);
+        setImportMessage(`${event.target.files[0].name}`);
+        if (event.target.files[0].type !== "application/JSON") {
+          setImportError(
+            <p className="import-error error-text">
+              File format is invalid. <br />
+              Only .json files are allowed.
+            </p>
+          );
+          return;
+        }
+        setImportError(`Error reading JSON`);
       }
     };
 
@@ -43,7 +56,7 @@ function Data(props) {
   return (
     <div className="data-container">
       <div className="flex-column gap-15">
-        <p>Export and Import saved Items.</p>
+        <p className="text-center">Export and Import saved Items.</p>
         <div className="button-row">
           <button className="primary-button" onClick={handleExportClick}>
             Export
@@ -62,6 +75,7 @@ function Data(props) {
           />
           <p>{importMessage}</p>
         </div>
+        {importError}
       </div>
     </div>
   );
